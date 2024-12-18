9e46739fe416876840e5c2468ef59670bbdb0c89
AnhTuan1678 <21020397@vnu.edu.vn>
2024-12-18 17:16:36 +0700

    init

---
+++ config/db.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config({ path: './main.env' });
+
+// const host = $MYSQL_SERVICE_HOST; hoặc process.env.DB_HOST
+const config = {
+  host: process.env.MYSQL_SERVICE_HOST || process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.MYSQL_SERVICE_PORT || process.env.DB_PORT,
+};
+const db = mysql.createPool(config);
+
+// Kiểm tra kết nối
+(async () => {
+  try {
+    const connection = await db.getConnection();
+    await connection.ping();
+    console.log(`Kết nối đến MySQL thành công: ${connection.config.database}`);
+    connection.release();
+  } catch (error) {
+    console.error(`Kết nối đến MySQL thất bại: ${error.message}`);
+  }
+})();
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const adminWarehouseRoutes = require('./routes/adminWarehouseRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const staffRoutes = require('./routes/staffRoutes');
+const statementRoutes = require('./routes/statementRoutes');
+const requestRoutes = require('./routes/requestRoutes');
+const logRoutes = require('./routes/logsRoutes');
+const db = require('./config/db');
+require('dotenv').config({ path: './main.env' });
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/auth', authRoutes);
+app.use('/adminW', adminWarehouseRoutes);
+app.use('/admin', adminRoutes);
+app.use('/staff', staffRoutes);
+app.use('/statement', statementRoutes);
+app.use('/adRequest', requestRoutes);
+app.use('/adLogs', logRoutes);
+
+// Giao diện
+app.use(express.static('build'));
+const CLIENT_PATH = 'build/index.html';
+app.get('*', (req, res) => {
+  res.sendFile(CLIENT_PATH, { root: __dirname });
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
